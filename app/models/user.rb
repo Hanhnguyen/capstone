ed96889f8bee31ce0abf5ce6052deeb4db2ee4c6
Hanh Nguyen <hanhn13@stanford.edu>
2013-06-03 16:45:51 -0700

    Add Avatar to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable #:recoverable,
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
   # attr_accessible :title, :body
 
+  validates_attachment :avatar, presence: true, content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            										size: { less_than: 5.megabytes }
   has_many :cards
-end
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50>" }
+ end
