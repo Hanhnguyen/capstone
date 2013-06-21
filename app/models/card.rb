83ff4f7a110072cc90ee21b4ba22b2ef716ba80e
Hanh Nguyen <hanhn13@stanford.edu>
2013-06-21 16:52:15 +0700

    Add maps to cities

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,18 +6,16 @@ class Card < ActiveRecord::Base
   validates :description, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
-  															content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
- 																size: { less_than: 5.megabytes }
+                                content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                                size: { less_than: 5.megabytes }
   belongs_to :user #foreign key- user_id
   belongs_to :city #foreign key- city_id
   belongs_to :category
   has_one :country, :through => :city
 
   has_attached_file :image, styles: { medium: "320x240>", large: "640x480>"}
-	def image_remote_url=(url_value)
+  def image_remote_url=(url_value)
     self.image = URI.parse(url_value) unless url_value.blank?
     super
   end
 end
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611015557) do
+ActiveRecord::Schema.define(:version => 20130621083632) do
 
   create_table "cards", :force => true do |t|
     t.string   "description"
